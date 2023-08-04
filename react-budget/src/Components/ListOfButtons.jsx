e5f455ef8376d4d205910d3d493e598d2ac8729e
Noé Ibáñez Torres <132618491+Noesersi@users.noreply.github.com>
2023-08-04 18:54:52 +0000

    first button commits

---
+++ react-budget/src/Components/ListOfButtons.jsx
@@ -0,0 +1,7 @@
+export default function ListOfButtons(Buttons) {
+    return (
+        <div className="btn-group-vertical" role="group" aria-label="Vertical button group">
+            {Buttons}
+        </div>  
+    )
+}
