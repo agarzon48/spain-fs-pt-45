5b9d892ee34bdfcf6eb2b556c2f5d743ea5b93b0
bruizpalen <115789234+bruizpalen@users.noreply.github.com>
2023-08-09 18:07:14 +0000

    Added map and digits logic

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -2,29 +2,14 @@ import CounterBG from "./components/CounterBG"
 import Card from "./components/Card"
 
 function App({ counter, firstDigit, secondDigit }) {
+ const digits = counter.toString().padStart(6, '0').split('');
  return (
     <CounterBG color="black">
-        <Card>
-            0
-        </Card>
-        <Card>
-            0
-        </Card>
-        <Card>
-            0
-        </Card>
-        <Card>
-            0
-        </Card>
-        <Card>
-            0
-        </Card>
-        <Card>
-            {secondDigit}
-        </Card>
-        <Card>
-            {firstDigit}
-        </Card>
+        {digits.map((digit, index) => (
+            <Card key={index}>
+                {digit}
+            </Card>
+        ))}
     </CounterBG>
  )
 }
--- counter/src/main.jsx
+++ counter/src/main.jsx
@@ -6,7 +6,7 @@ let counter = 0;
 setInterval(() => {
     const firstDigit = Math.floor((counter/1)%10);
     const secondDigit = Math.floor((counter/10)%10);
-
+    
 ReactDOM.createRoot(document.getElementById('root')).render(
     <App counter={counter} firstDigit={firstDigit} secondDigit={secondDigit} />
 )
